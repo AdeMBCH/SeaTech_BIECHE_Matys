92787d3da49f86e4fdc28dfb15acb2af61b9e52f
iutgeiitoulonE105 <touloniut@gmail.com>
2025-01-14 18:25:45 +0100

    début interface

--- InterfaceRobot/RobotInterfaceBIECHE/RobotInterfaceBIECHE/MainWindow.xaml.cs
+++ InterfaceRobot/RobotInterfaceBIECHE/RobotInterfaceBIECHE/MainWindow.xaml.cs
@@ -20,5 +20,55 @@ namespace RobotInterfaceBIECHE
         {
             InitializeComponent();
         }
+
+        bool isRoyalBlue = false;
+
+
+        private void SendMessage()
+        {
+            string toSend = textBoxEmission.Text;
+
+            bool isEmpty = false;
+
+            if (toSend == "")
+            {
+                isEmpty = true;
+            }
+
+            if (!isEmpty)
+            {
+                textBoxReception.Text += "Reçu : " + toSend + "\n";
+
+                textBoxEmission.Clear();
+            }
+        }
+
+        private void ChangeButtonColor()
+        {
+            if (isRoyalBlue)
+            {
+                buttonEnvoyer.Background = Brushes.Beige;
+                isRoyalBlue = false;
+            }
+            else
+            {
+                buttonEnvoyer.Background = Brushes.RoyalBlue;
+                isRoyalBlue = true;
+            }
+        }
+
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e) // Cliquer plusieurs fois ne fait rien, le bouton ne reviens pas à la couleur de base par exemple.
+        {
+            ChangeButtonColor();
+            SendMessage();
+        }
+
+        private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+        }
     }
 }
