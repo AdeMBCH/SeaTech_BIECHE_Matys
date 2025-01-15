23ea4d08144c39fef5f53fbbe299ebeddd54c907
iutgeiitoulonE105 <touloniut@gmail.com>
2025-01-15 15:54:57 +0100

    Interface com

--- InterfaceRobot/RobotInterfaceBIECHE/RobotInterfaceBIECHE/MainWindow.xaml.cs
+++ InterfaceRobot/RobotInterfaceBIECHE/RobotInterfaceBIECHE/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO.Ports;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ExtendedSerialPort_NS;
+using System.Windows.Threading;
+
 
 namespace RobotInterfaceBIECHE
 {
@@ -16,31 +20,50 @@ namespace RobotInterfaceBIECHE
     /// </summary>
     public partial class MainWindow : Window
     {
+        ExtendedSerialPort serialPort1;
+        DispatcherTimer timerAffichage;
+        Robot robot = new Robot();
+
         public MainWindow()
         {
+
+            timerAffichage = new DispatcherTimer();
+            timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timerAffichage.Tick += TimerAffichage_Tick;
+            timerAffichage.Start();
+
+            serialPort1 = new ExtendedSerialPort("COM7", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+            serialPort1.Open();
             InitializeComponent();
+
+        }
+
+        private void TimerAffichage_Tick(object? sender, EventArgs e)
+        {
+            textBoxReception.Text += robot.receivedText;
+            robot.receivedText = string.Empty;
+        }
+        private void SerialPort1_DataReceived(object? sender, DataReceivedArgs e)
+        {
+            // textBoxReception.Text += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+            robot.receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+            for (int i = 0; i < 20; i++)
+            {
+                robot.byteListReceived.Enqueue(e.Data[i]);
+            }
         }
 
         bool isRoyalBlue = false;
 
-
         private void SendMessage()
         {
-            string toSend = textBoxEmission.Text;
 
-            bool isEmpty = false;
+            serialPort1.WriteLine(textBoxEmission.Text);
 
-            if (toSend == "")
-            {
-                isEmpty = true;
-            }
+            //textBoxReception.Text += "Reçu : " + toSend + "\n";
 
-            if (!isEmpty)
-            {
-                textBoxReception.Text += "Reçu : " + toSend + "\n";
-
-                textBoxEmission.Clear();
-            }
+            textBoxEmission.Clear();
         }
 
         private void ChangeButtonColor()
@@ -59,7 +82,7 @@ namespace RobotInterfaceBIECHE
 
         private void buttonEnvoyer_Click(object sender, RoutedEventArgs e) // Cliquer plusieurs fois ne fait rien, le bouton ne reviens pas à la couleur de base par exemple.
         {
-            ChangeButtonColor();
+            //ChangeButtonColor();
             SendMessage();
         }
 
@@ -70,5 +93,25 @@ namespace RobotInterfaceBIECHE
                 SendMessage();
             }
         }
+
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            textBoxReception.Clear();
+        }
+
+        private void buttonTest_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] byteList = new byte[20];
+            for (int i = 0; i < 20; i++)
+            {
+                byteList[i] = (byte)(2*i);
+            }
+
+            serialPort1.Write(byteList, 0, byteList.Length);
+
+        }
     }
 }
+
+
+
---
+++ InterfaceRobot/RobotInterfaceBIECHE/RobotInterfaceBIECHE/Robot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Robot
+{
+    public string receivedText = "";
+    public float distanceTelemetreDroit;
+    public float distanceTelemetreCentre;
+    public float distanceTelemetreGauche;
+    public Queue<byte> byteListReceived;
+    public Robot()
+    {
+        Queue<byte> byteListReceived = new Queue<byte>();
+    }
+}
